1c4c41f420ff2f43ad8a8c8f86a5ad58b3d3cb4f
Kamil_hyperbook <k.marczak@savicon.pl>
2026-05-22 15:23:36 +0200

    Add PCComponent.cs Add ComponentManufacturer.cs

---
+++ web_api_2/Model/PCComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace web_api_2.Models;
+
+public class PCComponent
+{
+    public int PCId { get; set; }
+    public PC PC { get; set; } = null!;
+
+    public string ComponentCode { get; set; } = null!;
+    public Component Component { get; set; } = null!;
+
+    public int Amount { get; set; }
+}

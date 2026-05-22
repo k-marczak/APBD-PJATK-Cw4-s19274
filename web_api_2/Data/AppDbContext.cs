71334ea7ffd955a2267f56e8a7435a8a77d41fcc
Kamil_hyperbook <k.marczak@savicon.pl>
2026-05-22 15:15:17 +0200

    Init commit

---
+++ web_api_2/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace web_api_2.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
---
+++ web_api_2/Model/Component.cs
@@ -0,0 +1,16 @@
+﻿namespace web_api_2.Models;
+
+public class Component
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+
+    public int ComponentManufacturerId { get; set; }
+    public ComponentManufacturer Manufacturer { get; set; } = null!;
+
+    public int ComponentTypeId { get; set; }
+    public ComponentType Type { get; set; } = null!;
+
+    public ICollection<PCComponent> PCComponents { get; set; } = new List<PCComponent>();
+}

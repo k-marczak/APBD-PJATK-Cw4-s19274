606589c56cfe76be23ac20f126c0e61e06d72983
Kamil_hyperbook <k.marczak@savicon.pl>
2026-05-22 15:46:59 +0200

    Add PcDtos.cs

---
+++ web_api_2/DTOs/PcDtos.cs
@@ -0,0 +1,71 @@
+﻿namespace web_api_2.DTOs;
+
+public class PcResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
+
+public class CreatePcDto
+{
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
+
+public class UpdatePcDto
+{
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
+
+public class PcWithComponentsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+
+    public List<PcComponentDto> Components { get; set; } = new();
+}
+
+public class PcComponentDto
+{
+    public int Amount { get; set; }
+    public ComponentDto Component { get; set; } = null!;
+}
+
+public class ComponentDto
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public ComponentManufacturerDto Manufacturer { get; set; } = null!;
+    public ComponentTypeDto Type { get; set; } = null!;
+}
+
+public class ComponentManufacturerDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public DateTime FoundationDate { get; set; }
+}
+
+public class ComponentTypeDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string Name { get; set; } = null!;
+}
